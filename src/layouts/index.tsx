013b60292586e018800c6646cea5fa4f9859e5ff
changnannan <xiaomiya2008@163.com>
2023-12-06 16:15:29 +0800

    'tijiao'

---
+++ src/layouts/index.tsx
@@ -0,0 +1,21 @@
+import { Link, Outlet } from 'umi';
+import styles from './index.less';
+
+export default function Layout() {
+  return (
+    <div className={styles.navs}>
+      <ul>
+        <li>
+          <Link to="/">Home测试</Link>
+        </li>
+        <li>
+          <Link to="/docs">Docs测试</Link>
+        </li>
+        <li>
+          <a href="https://github.com/umijs/umi">Github</a>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
+}
